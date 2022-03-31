6d0385a63fc4aef85b838f4880340cd3c5bd9764
Changsik Yoon <dbs2636@gmail.com>
2022-04-01 00:40:57 +0900

    feat: Footer에 아이콘 추가 및 tailwind로 스타일링

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,15 +1,28 @@
 import React from "react";
-import styles from "./styles/footer.module.scss";
-import { AiOutlineMail } from "react-icons/ai";
+import { HiOutlineMail } from "react-icons/hi";
+import { AiOutlineInstagram } from "react-icons/ai";
 
 const Footer = () => {
   return (
     <footer className="bg-gray-800 text-slate-400">
-      <div className="">
-        <div className="">footer 설명 섹션</div>
+      <div className="px-4 py-5 sm:py-16 flex flex-row">
+        <div className="basis-2/3">
+          보통 여기에 주소와 전화 번호, 이메일 주소 등 정보가 들어갑니다.
+        </div>
 
-        <div className="">
-          <AiOutlineMail />
+        <div className="basis-1/3 text-right text-2xl">
+          <a href="mailto:dbs2636@gmail.com">
+            <HiOutlineMail
+              className="inline rounded-full bg-gray-600 p-2 hover:bg-gray-400 hover:text-gray-600 ml-2"
+              size="2.5rem"
+            />
+          </a>
+          <a href="mailto:dbs2636@gmail.com">
+            <AiOutlineInstagram
+              className="inline rounded-full bg-gray-600 p-2 hover:bg-gray-400 hover:text-gray-600 ml-2"
+              size="2.5rem"
+            />
+          </a>
         </div>
       </div>
     </footer>

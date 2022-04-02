8a17304ffe822caa330110ea6ad26c0b4d8dd323
Changsik Yoon <dbs2636@gmail.com>
2022-04-02 22:22:28 +0900

    feat: Add comingSoon 페이지 내용 추가

--- src/pages/comingSoon.tsx
+++ src/pages/comingSoon.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const ComingSoon = () => {
-  return <div>ComingSoon</div>;
+  return (
+    <section className="flex items-center justify-center h-screen text-center">
+      <div className="text-8xl font-bold rounded-full bg-white px-5 py-10 select-none">
+        Coming Soon!
+      </div>
+    </section>
+  );
 };
 
 export default ComingSoon;

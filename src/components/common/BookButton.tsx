c8997582b1292cf03e095dbe03add7f87c5baa2c
Changsik Yoon <dbs2636@gmail.com>
2022-04-03 22:54:00 +0900

    feat: Add BookButton 스타일링 추가

--- src/components/common/BookButton.tsx
+++ src/components/common/BookButton.tsx
@@ -3,9 +3,11 @@ import React from "react";
 
 const BookButton = () => {
   return (
-    <div>
+    <div className="text-center bg-white p-5 sticky bottom-0">
       <Link href="/">
-        <a>예매하기</a>
+        <a className="py-3 px-20 bg-red-400 hover:bg-red-300 transition-colors text-white rounded-lg text-xl">
+          예매하기
+        </a>
       </Link>
     </div>
   );

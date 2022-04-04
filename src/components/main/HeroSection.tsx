8dfdc3479ae3f84c4d5c42ef4e3e32365955b676
Changsik Yoon <dbs2636@gmail.com>
2022-04-05 00:41:08 +0900

    feat: Add 히어로섹션 이미지 대체 텍스트 추가

--- src/components/main/HeroSection.tsx
+++ src/components/main/HeroSection.tsx
@@ -4,8 +4,9 @@ import React from "react";
 import IconGroup from "../common/IconGroup";
 
 import bgMain from "@/images/background_main.jpg";
-import musical from "@/images/musical.png";
+import playInfo from "@/images/play_info.png";
 import line from "@/images/line.png";
+import musical from "@/images/musical.png";
 import title from "@/images/title.png";
 import location from "@/images/location.png";
 
@@ -23,20 +24,24 @@ const HeroSection = () => {
           className="object-bottom sm:object-center"
         />
         <div className="absolute w-full text-ivory text-center text-md sm:text-lg top-[35%] left-1/2 translate-x-[-50%] translate-y-[-50%] select-none">
-          <p className="text-sm font-light">
-            연세대학교 뮤지컬 소모임 ma:he 봄 공연
-          </p>
+          <div className="mx-24">
+            <Image src={playInfo} alt="극단 정보" quality={100} />
+          </div>
           <div className="my-3">
             <Image src={line} alt="line" quality={100} />
           </div>
           <div className="mx-24 my-5">
-            <Image src={musical} alt="musical" quality={100} />
+            <Image src={musical} alt="뮤지컬" quality={100} />
           </div>
           <div className="mx-12 my-5">
-            <Image src={title} alt="title" quality={100} priority />
+            <Image src={title} alt="어쩌다 해피엔딩" quality={100} priority />
           </div>
           <div className="mx-20">
-            <Image src={location} alt="location" quality={100} />
+            <Image
+              src={location}
+              alt="2022년 5월 12일 ~ 15일 북아현 아트홀"
+              quality={100}
+            />
           </div>
 
           <div className="mt-10 ">

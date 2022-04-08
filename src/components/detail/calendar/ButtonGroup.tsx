e6a4818bae2b68e2407d84031c208edea605a639
Changsik Yoon <dbs2636@gmail.com>
2022-04-09 01:41:08 +0900

    fix: Update div 대신 label에 스타일 적용

--- src/components/detail/calendar/ButtonGroup.tsx
+++ src/components/detail/calendar/ButtonGroup.tsx
@@ -8,40 +8,42 @@ const ButtonGroup = () => {
 
   return (
     <section className="flex flex-row gap-3 my-6 font-semibold text-sm">
-      <div
+      <input
+        type="radio"
+        name="time"
+        id="1400"
+        value="1400"
+        className="hidden"
+        onChange={onChangeRadio}
+      />
+      <label
+        htmlFor="1400"
         className={`basis-1/2 border-2 rounded-lg text-center py-3 ${
           selected === 1400
             ? "text-navy border-navy"
             : "text-lightGray border-lightGray"
         }`}
       >
-        <input
-          type="radio"
-          name="time"
-          id="1400"
-          value="1400"
-          className="hidden"
-          onChange={onChangeRadio}
-        />
-        <label htmlFor="1400">1회 14:00</label>
-      </div>
-      <div
+        1회 14:00
+      </label>
+      <input
+        type="radio"
+        name="time"
+        id="1900"
+        value="1900"
+        className="hidden"
+        onChange={onChangeRadio}
+      />
+      <label
+        htmlFor="1900"
         className={`basis-1/2 border-2 rounded-lg text-center py-3 ${
           selected === 1900
             ? "text-navy border-navy"
             : "text-lightGray border-lightGray"
         }`}
       >
-        <input
-          type="radio"
-          name="time"
-          id="1900"
-          value="1900"
-          className="hidden"
-          onChange={onChangeRadio}
-        />
-        <label htmlFor="1900">2회 19:00</label>
-      </div>
+        2회 19:00
+      </label>
     </section>
   );
 };

7aaa36c35513abf21770fb368927ac66b6f2aeba
Changsik Yoon <dbs2636@gmail.com>
2022-04-10 23:19:10 +0900

    feat: Add MusicalInfo 탭 내용 이미지 추가

--- src/components/detail/tabContents/MusicalInfo.tsx
+++ src/components/detail/tabContents/MusicalInfo.tsx
@@ -1,7 +1,13 @@
+import Image from "next/image";
 import React from "react";
+import about from "@/images/about.jpg";
 
 const MusicalInfo = () => {
-  return <div>MusicalInfo</div>;
+  return (
+    <article>
+      <Image src={about} alt="공연 소개" placeholder="blur" quality={100} />
+    </article>
+  );
 };
 
 export default MusicalInfo;

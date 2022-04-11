26fedec5ffe991c7489ab598d52f2c5539daef9f
Changsik Yoon <dbs2636@gmail.com>
2022-04-11 23:24:49 +0900

    feat: Update comingSoon 스타일 수정

--- src/pages/comingSoon.tsx
+++ src/pages/comingSoon.tsx
@@ -1,34 +1,17 @@
 import MetaTags from "@/components/common/MetaTags";
 import { useRouter } from "next/router";
-import React, { useEffect } from "react";
-import { collection, getDocs, query, where } from "firebase/firestore";
-import { db } from "@/firebase/firestore";
+import React from "react";
 import { NextPage } from "next";
 
 const ComingSoon: NextPage = () => {
   const { pathname } = useRouter();
-
-  useEffect(() => {
-    const getQs = async () => {
-      const ref = collection(db, "musicalInfo");
-      const q = query(ref, where("customers", "==", 0));
-
-      const querySnapshot = await getDocs(q);
-
-      querySnapshot.forEach((doc) => {
-        console.log("doc.id, doc.data()", doc.id, doc.data());
-      });
-    };
-
-    getQs();
-  }, []);
 
   return (
     <>
       <MetaTags subTitle="ComingSoon" pathname={pathname} />
 
       <section className="flex items-center justify-center h-screen text-center">
-        <div className="text-8xl font-bold rounded-full bg-pink px-5 py-10 select-none">
+        <div className="text-4xl font-bold px-5 py-10 select-none">
           Coming Soon!
         </div>
       </section>

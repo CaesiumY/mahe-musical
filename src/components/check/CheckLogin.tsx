d542c58405daf9ab12d9fb12ae6dfb8fb0a42c62
Changsik Yoon <dbs2636@gmail.com>
2022-04-12 01:46:57 +0900

    feat: Update CheckLogin 마크업 추가

--- src/components/check/CheckLogin.tsx
+++ src/components/check/CheckLogin.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { TicketsType } from "./types/types";
 
 interface CheckLoginProps {
@@ -6,7 +6,54 @@ interface CheckLoginProps {
 }
 
 const CheckLogin = ({ setData }: CheckLoginProps) => {
-  return <section>CheckLogin</section>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const onClickCheck = () => {
+    if (!name) return alert("이름을 입력해주세요!");
+    if (!email) return alert("이메일을 입력해주세요!");
+
+    console.log("name, email", name, email);
+  };
+
+  return (
+    <div className="flex flex-col gap-6 w-1/3 min-w-[300px] max-w-md">
+      <div className="flex flex-col gap-3">
+        <label htmlFor="customerName" className="font-bold">
+          이름
+        </label>
+        <input
+          className="border border-lightGray rounded-lg p-4 w-full"
+          type="text"
+          name="customerName"
+          id="customerName"
+          placeholder="ex) 김승현"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+      </div>
+      <div className="flex flex-col gap-3">
+        <label htmlFor="customerEmail" className="font-bold">
+          이메일
+        </label>
+        <input
+          className="border border-lightGray rounded-lg p-4 w-full"
+          type="email"
+          name="customerEmail"
+          id="customerEmail"
+          placeholder="ex) oliver@mahe.com"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </div>
+      <button
+        className="text-white bg-pink rounded-lg py-3 px-12 mx-auto mt-7 font-bold"
+        onClick={onClickCheck}
+      >
+        예매 확인하기
+      </button>
+    </div>
+  );
 };
 
 export default CheckLogin;

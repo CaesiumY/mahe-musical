2c7455447c71356f28f16a243071bc1748474616
Changsik Yoon <dbs2636@gmail.com>
2022-04-13 02:51:33 +0900

    feat: Update ProcessInfoBand에 나타나는 티켓 정보 수정

--- src/components/process/ProcessInfoBand.tsx
+++ src/components/process/ProcessInfoBand.tsx
@@ -12,22 +12,26 @@ const ProcessInfoBand = ({ ticketInfo }: ProcessInfoBandProps) => {
   const { musicalDate } = query;
   const { normal, wheelChair, barrierFree } = ticketInfo;
 
+  const day = musicalDate?.slice(0, 2);
+  const hour = musicalDate?.slice(2, 4);
+  const minute = musicalDate?.slice(-2);
+
   return (
     <div className="bg-mint-bg text-dark w-full">
       <div className="max-w-[80%] mx-auto py-8 flex flex-row gap-1">
-        <p>2022-05-{musicalDate?.slice(0, 2)}</p>
+        <p>2022-05-{day}</p>
         <span>|</span>
         <p>
-          {musicalDate?.slice(2, 4)}:{musicalDate?.slice(-2)}
+          {hour === "14" || minute === "30" ? "1회" : "2회"} {hour}:{minute}
         </p>
         <span>|</span>
         <p>{castingTable[musicalDate as keyof typeof castingTable]}</p>
-        {Object.values(ticketInfo).includes(0) ? "" : <span>|</span>}
+        {Object.values(ticketInfo).some((v) => v !== 0) ? <span>|</span> : ""}
         <p>
           {normal ? `일반석 ${normal}매` : ""}
-          {normal ? `, ` : ""}
+          {normal && wheelChair ? `, ` : ""}
           {wheelChair ? `휠체어석 ${wheelChair}매` : ""}
-          {wheelChair ? `, ` : ""}
+          {(normal || wheelChair) && barrierFree ? `, ` : ""}
           {barrierFree ? `배리어프리석 ${barrierFree}매` : ""}
         </p>
       </div>

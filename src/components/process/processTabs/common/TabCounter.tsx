ed7ff6aec8a113a5d782e887f5fb42adbd52da0c
Changsik Yoon <dbs2636@gmail.com>
2022-04-13 02:40:47 +0900

    style: Update 카운트 버튼 스타일 업데이트

--- src/components/process/processTabs/common/TabCounter.tsx
+++ src/components/process/processTabs/common/TabCounter.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaMinus, FaPlus } from "react-icons/fa";
 
 interface TabCounterProps {
   children: React.ReactNode;
@@ -17,9 +18,19 @@ const TabCounter = ({
     <div className="flex flex-row justify-between py-4 border-b border-lightGray">
       {children}
       <div className="flex flex-row">
-        <button onClick={onClickMinus}>-</button>
-        <span>{value}</span>
-        <button onClick={onClickPlus}>+</button>
+        <button
+          className="font-bold text-ivory-bg bg-lightGray rounded-full w-6 h-6"
+          onClick={onClickMinus}
+        >
+          <FaMinus className="m-auto" />
+        </button>
+        <span className="font-bold w-9 text-center">{value}</span>
+        <button
+          className="font-bold text-ivory-bg bg-lightGray rounded-full w-6 h-6"
+          onClick={onClickPlus}
+        >
+          <FaPlus className="m-auto" />
+        </button>
       </div>
     </div>
   );

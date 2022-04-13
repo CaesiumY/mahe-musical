f772510e319e6d8d32d0adc68665ecda2369b293
Changsik Yoon <dbs2636@gmail.com>
2022-04-14 03:18:24 +0900

    feat: Update sumValue를 추가하여 매 탭마다 수량 체크

--- src/components/process/ProcessContainer.tsx
+++ src/components/process/ProcessContainer.tsx
@@ -1,9 +1,10 @@
 import { DISCOUNTED_SEAT_PRICE, NOMAL_SEAT_PRICE } from "@/constants/constants";
 import React, { useMemo, useState } from "react";
-import { PriceType, SeatsType } from "../../types/types";
+import { PriceType, SeatsType, UserInfoType } from "../../types/types";
 import ProcessInfoBand from "./ProcessInfoBand";
 import DiscountSelect from "./processTabs/DiscountSelect";
 import SeatTypeSelect from "./processTabs/SeatTypeSelect";
+import UserInfo from "./processTabs/UserInfo";
 
 const ProcessContainer = () => {
   const [currentTab, setCurrentTab] = useState(0);
@@ -20,21 +21,29 @@ const ProcessContainer = () => {
     other: 0,
   });
 
+  const [userInfo, setUserInfo] = useState<UserInfoType>();
+
   const toNextTab = () => setCurrentTab((value) => value + 1);
+  const sumValue = (payload: object) =>
+    Object.values(payload).reduce((a, b) => a + b, 0);
 
   // seat logic
-  const onChangeSeatCount = (payload: SeatsType) => {
-    setSeatCount(payload);
-    toNextTab();
-  };
-
   const totalTickets = useMemo(
     () => Object.values(seatCount).reduce((a, b) => a + b, 0),
     [seatCount]
   );
 
+  const onChangeSeatCount = (payload: SeatsType) => {
+    if (sumValue(payload) === 0) return alert("1개 이상의 티켓을 넣어주세요!");
+    setSeatCount(payload);
+    toNextTab();
+  };
+
   // price logic
   const onChangePriceCount = (payload: PriceType) => {
+    if (sumValue(payload) !== totalTickets)
+      return alert("수량이 맞지 않습니다!");
+
     setPriceCount(payload);
     toNextTab();
   };
@@ -46,6 +55,12 @@ const ProcessContainer = () => {
     [priceCount]
   );
 
+  // userInfo logic
+  const onChangeUserInfo = (payload: UserInfoType) => {
+    setUserInfo(payload);
+    toNextTab();
+  };
+
   const tabs = [
     <SeatTypeSelect
       key="seatTypeSelect"
@@ -56,6 +71,7 @@ const ProcessContainer = () => {
       totalTickets={totalTickets}
       onChangePriceCount={onChangePriceCount}
     />,
+    <UserInfo key="userInfo" onChangeUserInfo={onChangeUserInfo} />,
   ];
 
   return (

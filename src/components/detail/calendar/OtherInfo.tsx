3b6e44cac00048f3bf0fb6f1a19a79e8430cea90
Changsik Yoon <dbs2636@gmail.com>
2022-04-16 03:25:49 +0900

    feat: Update 남은 좌석 수 타이핑 업데이트

--- src/components/detail/calendar/OtherInfo.tsx
+++ src/components/detail/calendar/OtherInfo.tsx
@@ -2,24 +2,25 @@ import React, { useEffect, useMemo, useRef, useState } from "react";
 import {
   BARRIER_FREE_SEAT_COUNT,
   castingTable,
-  collectionNames,
   NORMAL_SEAT_COUNT,
   WHEEL_CHARIR_SEAT_COUNT,
 } from "@/constants/constants";
-import { MusicalInfoType } from "@/types/types";
+import { SeatsType } from "@/types/types";
 import { onValue, ref } from "firebase/database";
 import { realtime } from "@/firebase/realtime";
 
-interface OtherInfoProps {
-  timeId: keyof typeof castingTable;
-}
+type CastingTableType = keyof typeof castingTable;
 
-interface MusicalDataType {
-  [key: string]: MusicalInfoType;
+type MusicalDataType = {
+  [key in CastingTableType]: SeatsType;
+};
+
+interface OtherInfoProps {
+  timeId: CastingTableType;
 }
 
 const OtherInfo = ({ timeId }: OtherInfoProps) => {
-  const [musicalData, setMusicalData] = useState<MusicalDataType>({});
+  const [musicalData, setMusicalData] = useState<MusicalDataType>();
 
   useEffect(() => {
     const getMusicalData = async () => {
@@ -38,11 +39,16 @@ const OtherInfo = ({ timeId }: OtherInfoProps) => {
   }, []);
 
   const remainedSeats = useMemo(
-    () => ({
-      normal: NORMAL_SEAT_COUNT - musicalData[timeId]?.normal,
-      wheelChair: WHEEL_CHARIR_SEAT_COUNT - musicalData[timeId]?.wheelChair,
-      barrierFree: BARRIER_FREE_SEAT_COUNT - musicalData[timeId]?.barrierFree,
-    }),
+    () =>
+      musicalData
+        ? {
+            normal: NORMAL_SEAT_COUNT - musicalData[timeId]?.normal,
+            wheelChair:
+              WHEEL_CHARIR_SEAT_COUNT - musicalData[timeId]?.wheelChair,
+            barrierFree:
+              BARRIER_FREE_SEAT_COUNT - musicalData[timeId]?.barrierFree,
+          }
+        : {},
     [musicalData, timeId]
   );
   const currentCasting = useMemo(() => castingTable[timeId], [timeId]);

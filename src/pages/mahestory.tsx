22641c253c22895b5367dbf4de348c13555fedb4
Changsik Yoon <dbs2636@gmail.com>
2022-05-11 16:49:43 +0900

    feat: Add 마헤 스토리 이미지 추가

--- src/pages/mahestory.tsx
+++ src/pages/mahestory.tsx
@@ -1,7 +1,10 @@
 import MetaTags from "@/components/common/MetaTags";
-import RedirectComingSoon from "@/components/common/RedirectComingSoon";
+import Image from "next/image";
 import { useRouter } from "next/router";
 import React from "react";
+import storyOne from "../images/story/1.webp";
+import storyTwo from "../images/story/2.webp";
+import storyThree from "../images/story/3.webp";
 
 const MaheStory = () => {
   const { pathname } = useRouter();
@@ -10,8 +13,29 @@ const MaheStory = () => {
     <>
       <MetaTags subTitle="마헤 이야기" pathname={pathname} />
 
-      <section>
-        <RedirectComingSoon />
+      <section className="max-w-xl mx-auto mt-24 flex flex-col">
+        <Image
+          src={storyOne}
+          alt="마헤 스토리 첫번째"
+          quality={100}
+          placeholder="blur"
+          layout="responsive"
+          priority
+        />
+        <Image
+          src={storyTwo}
+          alt="마헤 스토리 두번째"
+          quality={100}
+          placeholder="blur"
+          layout="responsive"
+        />
+        <Image
+          src={storyThree}
+          alt="마헤 스토리 세번째"
+          quality={100}
+          placeholder="blur"
+          layout="responsive"
+        />
       </section>
     </>
   );
